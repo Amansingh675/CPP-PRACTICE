438da6f3a23dcde578b34807ce23e76fcba186cc
aman7408 <amansingh747253@gmail.com>
2025-09-02 01:58:29 +0530

    Added Area of Rectangle program

---
+++ AREA_OF_RECTANGLE.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int length, width;
+    cout<<"enter the length"<<endl;
+    cin>>length;
+    cout<<"enter the width"<<endl;
+    cin>>width;
+    cout<<"Area of Rectangle is "<<length*width<<endl;
+    return 0;
+}
