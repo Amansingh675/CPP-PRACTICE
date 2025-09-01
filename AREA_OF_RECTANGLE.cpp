e6fb3d91705d8031e6b8dd06a3f2668b0a070c3a
aman7408 <amansingh747253@gmail.com>
2025-09-02 02:03:38 +0530

    Added perimeter in program

--- AREA_OF_RECTANGLE.cpp
+++ AREA_OF_RECTANGLE.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int length, width;
+    int length, width,perimeter;
     cout<<"enter the length"<<endl;
     cin>>length;
     cout<<"enter the width"<<endl;
     cin>>width;
     cout<<"Area of Rectangle is "<<length*width<<endl;
+    perimeter=2*(length+width);
+    cout<<"Perimeter of Rectangle is "<<perimeter<<endl;
     return 0;
 }
